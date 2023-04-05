4fb11bcee02b89a9f6e109deb3a84f51ffce946c
Christos Uster <christos.uster@gmail.com>
2023-04-05 23:26:37 +0600

    updates credentials

--- components/FirebaseConfig.js
+++ components/FirebaseConfig.js
@@ -4,11 +4,11 @@ import { getAuth } from "firebase/auth";
 
 const FirebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: "todo-list-with-login.firebaseapp.com",
-  projectId: "todo-list-with-login",
-  storageBucket: "todo-list-with-login.appspot.com",
-  messagingSenderId: "921218061866",
-  appId: "1:921218061866:web:508a3c4c369f92e6531553",
+  authDomain: "task-master-fbecc.firebaseapp.com",
+  projectId: "task-master-fbecc",
+  storageBucket: "task-master-fbecc.appspot.com",
+  messagingSenderId: "1017338456115",
+  appId: "1:1017338456115:web:a6be3771937a7620111b46",
 };
 
 const app = initializeApp(FirebaseConfig);
